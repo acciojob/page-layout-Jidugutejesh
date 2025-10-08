da94b3826ba32b4712cf2b1894182da1241129bf
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-08 14:56:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 
 import React from "react";
 import './../styles/App.css';
-
+function PageLayout({ header, footer, children }) {
+  return (
+    <div>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>{footer}</footer>
+    </div>
+  );
+}
 const App = () => {
   return (
     <div>
+     <PageLayout
+      header={<h1>My  Website is tejesh</h1>}
+      footer={<p>2025 My Website</p>}
+    >
+      <p>Welcome to the main content!</p>
+    </PageLayout>
         {/* Do not remove the main div */}
     </div>
   )
